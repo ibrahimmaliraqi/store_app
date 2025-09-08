d68a88667050df21338debd17a8ea9da89e99811
ibrahim mohammed <ibrahom0780@gmail.com>
2025-09-08 17:08:30 +0300

    create the store app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:store/Features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(StoreApp());
+}
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
