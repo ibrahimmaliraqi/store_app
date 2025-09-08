9a4b753a05b26d0b0d9b483a9472683bb3508f40
ibrahim mohammed <ibrahom0780@gmail.com>
2025-09-08 18:05:12 +0300

    fix error in style class

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 abstract class Styles {
   static TextStyle textStyle18 = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 18,
+  );
+  static TextStyle textStyle60 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 60,
+    fontFamily: GoogleFonts.lobster().fontFamily,
   );
   static TextStyle textStyle16 = TextStyle(
     fontWeight: FontWeight.w500,
