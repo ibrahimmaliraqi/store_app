0303f103eac18e30e1cb20efb0d679cc47817386
ibrahim mohammed <ibrahom0780@gmail.com>
2025-09-08 17:12:26 +0300

    create splash body

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:store/Features/splash/presentation/views/widgets/splash_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SplashBody(),
+    );
   }
 }
