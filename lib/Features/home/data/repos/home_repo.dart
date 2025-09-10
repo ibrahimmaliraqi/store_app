8919cb9156dad5380e90e436917043ec47958797
ibrahim mohammed <ibrahom0780@gmail.com>
2025-09-10 19:56:07 +0300

    create home repo

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,1 +1,7 @@
-abstract class HomeRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:store/Features/home/data/models/product_model/product_model.dart';
+import 'package:store/core/errors/failure.dart';
+
+abstract class HomeRepo {
+  Future<Either<ServerFailure, List<ProductModel>>> fetchProduct();
+}
