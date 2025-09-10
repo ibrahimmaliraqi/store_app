9e3c6418b1d3e26885f76aefc0cdd0bfa263307d
ibrahim mohammed <ibrahom0780@gmail.com>
2025-09-10 20:09:14 +0300

    half of home repo impl

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -2,11 +2,22 @@ import 'package:dartz/dartz.dart';
 import 'package:store/Features/home/data/models/product_model/product_model.dart';
 import 'package:store/Features/home/data/repos/home_repo.dart';
 import 'package:store/core/errors/failure.dart';
+import 'package:store/core/utils/app_services.dart';
 
 class HomeRepoImpl implements HomeRepo {
   @override
-  Future<Either<ServerFailure, List<ProductModel>>> fetchProduct() {
-    // TODO: implement fetchProduct
-    throw UnimplementedError();
+  Future<Either<ServerFailure, List<ProductModel>>> fetchProduct() async {
+    try {
+      var res = await AppServices.get(endPoint: "/products");
+      List<ProductModel> burager = [];
+      for (var element in res["products"]) {
+        burager.add(
+          ProductModel.fromJson(element),
+        );
+      }
+      return right(burager);
+    } catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
