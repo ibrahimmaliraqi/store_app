b7a4f5867ec79595b8d5862351180154bba5712e
ibrahim mohammed <ibrahom0780@gmail.com>
2025-09-11 10:39:53 +0300

    product cubit done

--- lib/Features/home/presentation/manager/product_cubit/product_cubit_cubit.dart
+++ lib/Features/home/presentation/manager/product_cubit/product_cubit_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:store/Features/home/data/models/product_model/product_model.dart';
+import 'package:store/Features/home/data/repos/home_repo.dart';
 
 part 'product_cubit_state.dart';
 
 class ProductCubitCubit extends Cubit<ProductCubitState> {
-  ProductCubitCubit() : super(ProductCubitInitial());
+  ProductCubitCubit(this.homeRepo) : super(ProductCubitInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchHomeProduct() async {
+    emit(ProductCubitLoading());
+    var result = await homeRepo.fetchProduct();
+    result.fold(
+      (failure) {
+        emit(ProductCubitFailure(failure.errMessage));
+      },
+      (burgers) {
+        emit(ProductCubitSuccess(burgers));
+      },
+    );
+  }
 }
--- lib/Features/home/presentation/manager/product_cubit/product_cubit_state.dart
+++ lib/Features/home/presentation/manager/product_cubit/product_cubit_state.dart
@@ -8,3 +8,17 @@ sealed class ProductCubitState extends Equatable {
 }
 
 final class ProductCubitInitial extends ProductCubitState {}
+
+final class ProductCubitLoading extends ProductCubitState {}
+
+final class ProductCubitSuccess extends ProductCubitState {
+  final List<ProductModel> burgers;
+
+  const ProductCubitSuccess(this.burgers);
+}
+
+final class ProductCubitFailure extends ProductCubitState {
+  final String errMessage;
+
+  const ProductCubitFailure(this.errMessage);
+}
